db4ed64c343dd9ed8a6f139f925ed2da21afd01c
dipakagrahari <79031639+dipakagrahari@users.noreply.github.com>
2021-05-07 19:09:09 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+
+from flask import Flask, jsonify
+import numpy
+import nilearn
+import timeit
+
+app = Flask(__name__)
+
+
+# @app.rout('/mlinfe',methods = ['POST'])
+
+@app.route('/module/<string:name>', methods=['GET'])
+def get(name):
+    if name == 'numpy':
+        a = dir(numpy)
+        return jsonify(a)
+    
+    if name == 'nilearn':
+        a = dir(nilearn)
+        return jsonify(a)
+    
+    if name == 'timeit':
+        a = dir(timeit)
+        return jsonify(a)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
